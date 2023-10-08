27872527fb06f197ba21e527ca930687e993ccf6
sergio-nunez <sergio.nunez@laposte.net>
2023-10-08 22:16:07 +0200

    Service: schedule init

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Schedule;
+use App\Service\ScheduleService;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,31 +16,11 @@ class HomeController extends AbstractController
         Route('/accueil', 'app_home_alt')
     ]
     //#[Route('/home', name: 'app_home')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(ScheduleService $scheduleService): Response
     {
-        $daysOfWeek = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche' ];
-        $repository = $entityManager->getRepository(Schedule::class);
-
-        $test = 'Lundi matin';
-
-        $shedules = $repository->findAll();
-
-        $morning = ['', '', '', '', '', '', ''];
-        $afternoon = ['', '', '', '', '', '', ''];
-
-        foreach ($shedules as $shedule) {
-            if($shedule->getServiceType() == 'Midi') {
-                $morning[$shedule->getDayOfWeek()] = $shedule->getServiceStart()->format('H:i'). ' - ' . $shedule->getServiceEnd()->format('H:i');
-            } else {
-                $afternoon[$shedule->getDayOfWeek()] =$shedule->getServiceStart()->format('H:i'). ' - ' . $shedule->getServiceEnd()->format('H:i');
-            }
-        }
-
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'daysOfWeek' => $daysOfWeek,
-            'morning' => $morning,
-            'afternoon' => $afternoon,
+            'schedules' => $scheduleService->getSchedules()
         ]);
     }
 }
