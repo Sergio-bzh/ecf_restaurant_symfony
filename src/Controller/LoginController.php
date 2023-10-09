75103d7bacd759858437dab690c9cb737d11d462
sergio-nunez <sergio.nunez@laposte.net>
2023-10-09 15:06:12 +0200

    Feat: init header

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
+use App\Service\ScheduleService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends AbstractController
+class LoginController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils,ScheduleService $scheduleService): Response
     {
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
@@ -21,7 +22,7 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('login/login.html.twig', ['last_username' => $lastUsername, 'error' => $error, 'schedules' => $scheduleService->getSchedules()]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
