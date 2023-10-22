b71ef130ff1855c0024057f34e1a77e2ae532da2
sergio-nunez <sergio.nunez@laposte.net>
2023-10-22 18:00:12 +0200

    Feat: display timeslices by selected service

--- public/js/script.js
+++ public/js/script.js
@@ -5,13 +5,13 @@ option.innerText = 'Travail'
 allergiesList.append(option)
 */
 
-// TODO : Géréer les heures début/fin de service dans les tranches horaires intermédiaires
+// TODO : Géréer les heures début/fin de service dans les tranches horaires intermédiaires histoire des 3/4 d'heures
 // TODO : Ne pas afficher la dernière heure de service
-// TODO : Afficher les minutes au lieu du numéro du quart d'heure exemple (12:15 au lieu de 12:1)
 // TODO : Bloquer la possibilité de choisir un jour passé
 
 let selector = window.document.getElementsByName('reservation[allergies]')
-toggleAllergies()
+let premierPassage = true
+
 function toggleAllergies() {
     const ALLERGIE_CHECKBOX_FIELD = window.document.getElementById('reservation_allergie')
     const ALLERGIES = window.document.getElementById('allergies_list')
@@ -32,11 +32,23 @@ async function getCreneaux() {
 //  Appel à l'API Back-End en dur pour les tests sur LOCALHOST !!
     const response = await fetch(`https://localhost:8000/api/timeSlice?date=${date.value}&service=${service.value}`)
     const creneaux = await response.json()
-
+console.log(creneaux)
     const CAPACITE = 50
     let allValeurs = [0, 0, 0]
     let indiceCumulPlaces = 3
-//    select.innerHTML = ''
+
+//  Constante créé à partir du tableau des quart d'heures (96 balise option) présentes dans le DOM
+    const ALLCRENEAUX = window.document.getElementById('reservation_meal_time').getElementsByTagName('option');
+
+// Boucle pour masquer les 96 balises option avant d'obtenir les heures à afficher dans le formulaire
+    if (!premierPassage) {
+        for (option of ALLCRENEAUX)
+           option.style.display = 'none'
+    }
+    premierPassage = false
+
+    let debutService = parseInt(creneaux.service_start.substring(11, 13))
+    let finService = parseInt(creneaux.service_end.substring(11, 13))
 
 // Boucle pour obtenir les heures à afficher dans le formulaire
     for (let dateH = creneaux.service_start.substring(11, 13); dateH < creneaux.service_end.substring(11, 13) ; dateH++) {
@@ -53,9 +65,7 @@ async function getCreneaux() {
             console.log(indiceCumulPlaces,allValeurs)
 
             if((allValeurs[indiceCumulPlaces]) != null && places_prises + parseInt(guest_number.value) + allValeurs[indiceCumulPlaces-1] + allValeurs[indiceCumulPlaces-2] + allValeurs[indiceCumulPlaces-3] <= CAPACITE ) {
-                let option = window.document.createElement('option')
-                option.innerText = `${dateH}:${quartHeure}`
-                select.appendChild(option)
+                ALLCRENEAUX[((debutService * 4) - 2) + indiceCumulPlaces].style.display = 'block';
             }
             quartHeure++
             indiceCumulPlaces++
@@ -63,8 +73,13 @@ async function getCreneaux() {
     }
 }
 
+/*
+ReMasquer les creneaux :
+Si guest_number change recalcule
 
-
+Si service change recalcule
+Si date change recalcule
+*/
 
 
 
