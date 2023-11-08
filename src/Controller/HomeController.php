231c12a9b03b9b777295d2e5e23eeb04a36964c1
sergio-nunez <sergio.nunez@laposte.net>
2023-11-08 21:44:58 +0100

    Pb persist & flush en BDD

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Schedule;
+use App\Repository\ScheduleRepository;
 use App\Service\ScheduleService;
+use DateTime;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,8 +19,27 @@ class HomeController extends AbstractController
         Route('/accueil', 'app_home_alt')
     ]
     //#[Route('/home', name: 'app_home')]
-    public function index(ScheduleService $scheduleService): Response
+    public function index(ScheduleService $scheduleService, EntityManagerInterface $entityManager): Response
     {
+
+//        $schedule = $scheduleRepo->find(1);
+        $schedule = $entityManager->getRepository(Schedule::class)->find(1);
+        echo '1 ====================>, <br>';
+        $newServiceStart = $schedule->getServiceStart();
+        echo '2 ====================>';
+//        dd($newServiceStart);
+        $transform = $newServiceStart->format('H:i');
+//        var_dump(date (strtotime($transform)));
+//        dd($transform);
+//        $newSetServiceStart = $schedule->setServiceStart(DateTime::createFromFormat('H:i', strtotime($transform)));
+        $schedule->setServiceStart($newServiceStart->modify(' + 5 hour'));
+        dd($schedule);
+//        var_dump('============>'. $newServiceStart->modify(' + 5 hour'));
+        $entityManager->persist($schedule);
+//        dd($truc);
+        $entityManager->flush();
+
+//        var_dump($newSetServiceStart);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'schedules' => $scheduleService->getSchedules()
